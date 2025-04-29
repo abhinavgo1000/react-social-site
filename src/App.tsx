2e09cb15ea4bfec4f292ce244a219b022b889bb2
Abhinav Goel <abhinavgo1000@gmail.com>
2025-04-29 14:40:06 -0600

    color mode toggle button added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ColorModeButton } from './components/ui/color-mode';
 import './App.css';
 
 const queryClient = new QueryClient();
@@ -10,6 +11,7 @@ function App() {
     return (
         <React.Fragment>
             <QueryClientProvider client={queryClient}>
+                <ColorModeButton />
                 <Router>
                     <Routes>
                         <Route path="/home" element={<div>Home</div>} />
